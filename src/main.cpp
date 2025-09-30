0209a8705eb6cbfed70c0cc96cf18c562241e579
Diogo <diogodomidacruz@gmail.com>
2025-09-30 15:46:49 -0300

    inclusão do arquivo main.cpp para referêcnia do programa principal

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,50 @@
-#include "SystemManager.h"
+#include "WiFiManager.h"
+#include "WebManager.h"
+#include "IMUManager.h"
+#include "FlightManager.h"
+
+WiFiManager wifi{};
+
+AsyncWebServer server(80);
+AsyncWebSocket socket("/ws");
+WebManager web(server, socket);
+
+IMUManager imu;
+FlightManager flight(imu);
+
+static unsigned long previousTelemetryTime = 0L;
+constexpr unsigned long TELEMETRY_INTERVAL = 1000;
+
+GPSData gpsData = {10.3983, -50.2392, 120.19};
 
 void setup() {
-    SystemManager::getInstance().setup();
+    Serial.begin(115200);
+    Serial.println();
+
+    flight.begin();
+    wifi.begin();
+    web.begin();
+
+    delay(500);
+    
+    web.cacheTelemetry(gpsData, wifi.getRSSI(), flight.getState());
+
+    previousTelemetryTime = millis();
 }
 
 void loop() {
-    SystemManager::getInstance().loop();
+    web.update();
+    wifi.update();
+
+    JoystickData joystickData = web.getData();
+    bool stateChangeRequested = web.hasStateChangeRequest();
+
+    flight.update(stateChangeRequested, joystickData);
+
+    if (stateChangeRequested || (millis() - previousTelemetryTime >= TELEMETRY_INTERVAL)) {
+        previousTelemetryTime = millis();
+
+        web.cacheTelemetry(gpsData, wifi.getRSSI(), flight.getState());
+        web.sendTelemetry(gpsData, wifi.getRSSI(), flight.getState());
+    }
 }

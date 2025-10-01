cea83a148bfdaaea91fe8daade2a7770503a2ec0
Diogo <diogodomidacruz@gmail.com>
2025-10-01 14:08:48 -0300

    New: namespace Flags criado.
    - Namespace responsável por armazenar as flags de retorno para valores
      inválidos ou erro em leitura de sensores. Inclui flags para sensores
      WiFi (RSSI) e GPS (latitude, longitude e altitude).

---
+++ include/Flags.h
@@ -0,0 +1,13 @@
+#ifndef FLAGS_H_
+#define FLAGS_H_
+
+#include <cstdint>
+
+namespace Flags {
+    constexpr int8_t WIFI_RSSI_INVALID = -128;
+    constexpr float GPS_LAT_INVALID = 0.0F;
+    constexpr float GPS_LON_INVALID = 0.0F;
+    constexpr float GPS_ALT_INVALID = 0.0F;
+}
+
+#endif

d93285ef37d9ad20e8664f44419c6d2a876d1cd7
Diogo <diogodomidacruz@gmail.com>
2025-11-25 19:15:50 -0300

    Update: Atualizando implemetação de IMUManager.
    -Corrigindo lógica de alguns métodos que utilziavam interrupções do
    sensor;
    -Ajustando offsets manualmente + calibrado em seguida.

--- lib/IMUManager/IMUManager.cpp
+++ lib/IMUManager/IMUManager.cpp
@@ -1,27 +1,34 @@
+#include "MPU6050_6Axis_MotionApps20.h"
 #include "IMUManager.h"
 #include "I2Cdev.h"
 #include "Wire.h"
 #include "Pins.h"
 
 namespace {
-    IMUManager* g_pInstance = nullptr;
-
     enum class Angles : uint8_t { YAW = 0, PITCH = 1, ROLL = 2 };
 
-    constexpr uint8_t HARDWARE_LOOPS = 10;
+    constexpr uint8_t HARDWARE_LOOPS = 20;
     constexpr uint16_t SOFTWARE_LOOPS = 200;
 }
 
-void IRAM_ATTR imu_isr_wrapper() {
-    if (g_pInstance) { g_pInstance->dmpDataReady(); }
+void IRAM_ATTR IMUManager::isr() {
+    if (g_localInstance) {
+        g_localInstance->m_interrupt = true; 
+    }
 }
 
-void IMUManager::dmpDataReady() { m_interrupt = true; }
+IMUManager* IMUManager::g_localInstance = nullptr;
 
-IMUManager::IMUManager() { g_pInstance = this; }
+IMUManager::IMUManager() :
+    m_gyroScaleDivisor(16.4F)
+{ 
+    g_localInstance = this; 
+}
 
 void IMUManager::begin() {
     setupConfig();
+    unsigned long timer = millis();
+    while (millis() - timer < 2000) yield();
     calibrateHardwareOffsets();
     while(!isDMPEnabled()) { yield(); }
     calibrateSoftwareOffsets();
@@ -31,9 +38,20 @@ void IMUManager::setupConfig() {
     Wire.begin(Pins::MPU::SDA_PIN, Pins::MPU::SCL_PIN);
     Wire.setClock(400000L);
     m_mpu.initialize();
-    m_mpu.setDLPFMode(MPU6050_DLPF_BW_42);
     pinMode(Pins::MPU::INTERRUPT_PIN, INPUT_PULLUP);
+
+    // Valores capturados em: 25/11/2025 
+    m_mpu.setXAccelOffset(-5008);
+    m_mpu.setYAccelOffset(-1863);
+    m_mpu.setZAccelOffset(178);
+    m_mpu.setXGyroOffset(1031);
+    m_mpu.setYGyroOffset(-125);
+    m_mpu.setZGyroOffset(18);
+
     m_devStatus = m_mpu.dmpInitialize();
+    m_mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_2000);
+    m_mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+    m_mpu.setDLPFMode(MPU6050_DLPF_BW_42);
 }
 
 void IMUManager::calibrateHardwareOffsets() {
@@ -46,7 +64,7 @@ bool IMUManager::isDMPEnabled() {
         return false;
     }
     m_mpu.setDMPEnabled(true);
-    attachInterrupt(digitalPinToInterrupt(Pins::MPU::INTERRUPT_PIN), imu_isr_wrapper, RISING);
+    attachInterrupt(digitalPinToInterrupt(Pins::MPU::INTERRUPT_PIN), isr, RISING);
     m_intStatus = m_mpu.getIntStatus();
     m_dmpReady = true;
     m_packetSize = m_mpu.dmpGetFIFOPacketSize();
@@ -57,21 +75,38 @@ void IMUManager::calibrateSoftwareOffsets() {
     uint16_t successfulReads = 0;
     IMUData rawReading{};
 
+    m_mpu.resetFIFO();
+    m_mpu.getIntStatus();
+    m_interrupt = false;
+
     for (uint16_t i = 0; i < SOFTWARE_LOOPS; i++) {
-        while (!m_interrupt) { yield(); }
+        unsigned long timeoutStart = millis();
+
+        while(m_mpu.getFIFOCount() < m_packetSize) {
+            yield();
+
+            if (millis() - timeoutStart > 100) {
+                break;
+            }
+        }
 
         if (readDMPData(rawReading)) {
             m_swOffsets.yaw += rawReading.yaw;
             m_swOffsets.pitch += rawReading.pitch;
             m_swOffsets.roll += rawReading.roll;
+            m_swOffsets.gyroX += rawReading.gyroX;
+            m_swOffsets.gyroY += rawReading.gyroY;
             m_swOffsets.gyroZ += rawReading.gyroZ;
             successfulReads++;
         }
     }
+
     if (successfulReads > 0) {
         m_swOffsets.yaw /= successfulReads;
         m_swOffsets.pitch /= successfulReads;
         m_swOffsets.roll /= successfulReads;
+        m_swOffsets.gyroX /= successfulReads;
+        m_swOffsets.gyroY /= successfulReads;
         m_swOffsets.gyroZ /= successfulReads;
     }
 }
@@ -84,24 +119,27 @@ bool IMUManager::readDMPData(IMUData& data) {
     if ((m_intStatus & 0x10) || m_fifoCount == 1024) {
         m_mpu.resetFIFO();
         return false;
-    } else if (m_intStatus & 0x02) {
-        if (m_fifoCount < m_packetSize) { return false; }
+    }
 
-        m_mpu.getFIFOBytes(m_fifoBuffer, m_packetSize);
+    if ((m_intStatus & 0x02) && (m_fifoCount >= m_packetSize)) {
+        while (m_fifoCount >= m_packetSize) {
+            m_mpu.getFIFOBytes(m_fifoBuffer, m_packetSize);
+            m_fifoCount -= m_packetSize;
+        }
 
         m_mpu.dmpGetQuaternion(&m_quaternion, m_fifoBuffer);
         m_mpu.dmpGetGravity(&m_gravity, &m_quaternion);
 
-        float ypr[3]{};
-        m_mpu.dmpGetYawPitchRoll(ypr, &m_quaternion, &m_gravity);
+        m_mpu.dmpGetYawPitchRoll(m_ypr, &m_quaternion, &m_gravity);
 
-        VectorInt16 gyro{};
-        m_mpu.dmpGetGyro(&gyro, m_fifoBuffer);
+        m_mpu.dmpGetGyro(&m_gyro, m_fifoBuffer);
 
-        data.yaw = ypr[static_cast<uint8_t>(Angles::YAW)] * 180/M_PI;
-        data.pitch = ypr[static_cast<uint8_t>(Angles::PITCH)] * 180/M_PI;
-        data.roll = ypr[static_cast<uint8_t>(Angles::ROLL)] * 180/M_PI;
-        data.gyroZ = static_cast<float>(gyro.z) / 131.0F;
+        data.yaw = m_ypr[static_cast<uint8_t>(Angles::YAW)] * 180/M_PI;
+        data.pitch = m_ypr[static_cast<uint8_t>(Angles::PITCH)] * 180/M_PI;
+        data.roll = m_ypr[static_cast<uint8_t>(Angles::ROLL)] * 180/M_PI;
+        data.gyroX = static_cast<float>(m_gyro.x) / m_gyroScaleDivisor;
+        data.gyroY = static_cast<float>(m_gyro.y) / m_gyroScaleDivisor;
+        data.gyroZ = static_cast<float>(m_gyro.z) / m_gyroScaleDivisor;
 
         return true;
     }
@@ -109,20 +147,32 @@ bool IMUManager::readDMPData(IMUData& data) {
 }
 
 void IMUManager::update() {
-    if (!m_interrupt) { return; }
+    if (!m_interrupt) { 
+        uint16_t currentFIFO = m_mpu.getFIFOCount();
+        if (currentFIFO < m_packetSize) {
+            return;
+        }
+    }
+
+    m_interrupt = false;
 
     IMUData rawReading{};
     if (readDMPData(rawReading)) {
-        m_mpuData.yaw = rawReading.yaw - m_swOffsets.yaw;
-        m_mpuData.pitch = rawReading.pitch - m_swOffsets.pitch;
-        m_mpuData.roll = rawReading.roll - m_swOffsets.roll;
-        m_mpuData.gyroZ = rawReading.gyroZ - m_swOffsets.gyroZ;
+        float tmpYaw = rawReading.yaw - m_swOffsets.yaw;
+        float tmpPitch = rawReading.pitch - m_swOffsets.pitch;
+        float tmpRoll = rawReading.roll - m_swOffsets.roll;
 
-        m_mpuData.pitch *= -1.00;
-        m_mpuData.gyroZ *= -1.00;
+        float tmpGyroX = rawReading.gyroX - m_swOffsets.gyroX;
+        float tmpGyroY = rawReading.gyroY - m_swOffsets.gyroY;
+        float tmpGyroZ = rawReading.gyroZ - m_swOffsets.gyroZ;
+
+        m_mpuData.roll = tmpRoll;
+        m_mpuData.gyroX = tmpGyroX;
+
+        m_mpuData.pitch = tmpPitch;
+        m_mpuData.gyroY = -tmpGyroY;
+
+        m_mpuData.yaw = tmpYaw;
+        m_mpuData.gyroZ = -tmpGyroZ;
     }
-}
-
-IMUData IMUManager::getMPUData() const { 
-    return m_mpuData;
 }
--- lib/IMUManager/IMUManager.h
+++ lib/IMUManager/IMUManager.h
@@ -1,18 +1,18 @@
 #ifndef _IMU_MANAGER_H_
 #define _IMU_MANAGER_H_
 
-#include "MPU6050_6Axis_MotionApps20.h"
 #include "IMUData.h"
+#include "MPU6050.h"
 
 class IMUManager {
     private:
-
         IMUData m_swOffsets{};
         IMUData m_mpuData{};
 
         MPU6050 m_mpu{};
 
         volatile bool m_interrupt{};
+
         bool m_dmpReady{};
         uint8_t m_intStatus{};
         uint8_t m_devStatus{};
@@ -22,6 +22,15 @@ class IMUManager {
 
         Quaternion m_quaternion{};
         VectorFloat m_gravity{};
+        VectorInt16 m_gyro{};
+        float m_ypr[3]{};
+
+        float m_gyroScaleDivisor{};
+
+    private:
+
+        static IMUManager* g_localInstance;
+        static void IRAM_ATTR isr();
 
     private:
 
@@ -36,10 +45,10 @@ class IMUManager {
         IMUManager();
         void begin();
         void update();
-        IMUData getMPUData() const;
 
-    public:
-        void dmpDataReady();
+        inline const IMUData& getMPUData() const { 
+            return m_mpuData;
+        }
 
 };
 
